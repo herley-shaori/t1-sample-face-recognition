2f77ac29bdbc694754a2fffcf81ae17fcf38a134
herley <herley.shaori@gmail.com>
2023-12-27 13:08:30 +0700

    created uncofirmed sagemaker training job submit.

---
+++ sagemaker_train.py
@@ -0,0 +1,76 @@
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, precision_score, recall_score, f1_score
+
+# ... (previous code)
+
+# Split into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_train_padded, y_train, test_size=0.2, random_state=42)
+
+# Submit SageMaker training job
+import sagemaker
+from sagemaker.sklearn.estimator import SKLearn
+
+# Configure SageMaker session and role
+sagemaker_session = sagemaker.Session()
+role = sagemaker.get_execution_role()
+
+# Create SKLearn estimator
+estimator = SKLearn(
+    entry_point="train.py",  # Script containing model training code
+    source_dir=".",
+    role=role,
+    instance_count=1,
+    instance_type="ml.m5.xlarge",
+    framework_version="0.23-1",
+    py_version="py3",
+)
+
+# Train the model on SageMaker
+estimator.fit({"train": X_train, "test": X_test})
+
+# Deploy model to SageMaker endpoint
+predictor = estimator.deploy(initial_instance_count=1, instance_type="ml.t2.medium")
+
+# Evaluate model on SageMaker
+test_data = {"test": X_test}
+predictions = predictor.predict(test_data)
+
+# Calculate metrics
+# ... (code for confusion matrix, precision, recall, f1-score)
+
+# Register the model
+model_name = "face-recognition-model"
+model_data = estimator.model_data
+sagemaker_session.register_model(model_name, model_data)
+
+# Create model endpoint configuration
+endpoint_name = "face-recognition-endpoint"
+endpoint_config_name = "face-recognition-endpoint-config"
+model_package_arn = estimator.model_package_arn
+
+sagemaker_session.create_endpoint_config(
+    EndpointConfigName=endpoint_config_name,
+    ProductionVariants=[
+        {
+            "InstanceType": "ml.t2.medium",
+            "InitialInstanceCount": 1,
+            "ModelPackageArn": model_package_arn,
+            "VariantName": "AllTraffic",
+        }
+    ],
+)
+
+# Create endpoint
+sagemaker_session.create_endpoint(EndpointName=endpoint_name, EndpointConfigName=endpoint_config_name)
+
+
+# ... (code to load an image using OpenCV)
+
+# Send image to endpoint for prediction
+payload = np.expand_dims(img_array, axis=0).tolist()
+response = predictor.predict(payload)
+prediction = response["predictions"][0]  # Extract prediction
+print("Predicted class:", prediction)
